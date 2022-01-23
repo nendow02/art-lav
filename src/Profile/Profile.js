aef6e09259ef2d530205b1749362818fa5828692
Jiin <jiinkim02@gmail.com>
2022-01-22 18:44:16 -0800

    add pfp icons

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,30 +2,67 @@ import { useContext } from "react";
 import { LocationContext } from "../Location/LocationContext.js";
 import { getAuth, signOut } from "firebase/auth";
 import { AuthContext } from "../Authentication/AuthContext.js";
+import { ProfileContext } from "./ProfileContext.js";
+import backButton from "../img/back.svg";
+import signout from "../img/signout.svg";
+import map from "../img/map.svg";
+import "./profile.css";
 
 function Profile(props) {
   const { lat, lng, setIsMapOpen } = useContext(LocationContext);
   const { setIsSignedIn } = useContext(AuthContext);
+  const { setIsProfileOpen } = useContext(ProfileContext);
   const auth = getAuth();
 
   return (
     <div>
-      <div>this is the profile page uwu</div>
-      <button onClick={() => props.setIsProfileOpen(false)}>
-        go back to main page
-      </button>
-      <button onClick={() => setIsMapOpen(true)}>change location</button>
-      <button
+      <img
+        className="back-button"
+        src={backButton}
+        onClick={() => {
+          setIsProfileOpen(false);
+        }}
+      />
+      <img
+        className="map-button"
+        src={map}
+        onClick={() => {
+          setIsMapOpen(true);
+        }}
+      />
+      <img
+        className="profile-button"
+        src={signout}
         onClick={() => {
           signOut(auth).then(() => {
             console.log("signed out");
             setIsSignedIn(false);
           });
         }}
-      >
-        Sign out
-      </button>
+      />
+      <h2>Your Posts</h2>
     </div>
   );
 }
 export default Profile;
+
+{
+  /* <div>
+  {openedImage && (
+    <OutsideAlerter setOpenedImage={setOpenedImage}>
+      <Image img={openedImage} />
+    </OutsideAlerter>
+  )}
+
+  <div className={openedImage && "blur"}>
+    <h1>our app name owo</h1>
+    <img
+      className="profile-button"
+      src={profileImg}
+      onClick={() => setIsProfileOpen(true)}
+    />
+    <Upload />
+    {showImages()}
+  </div>
+</div> */
+}
